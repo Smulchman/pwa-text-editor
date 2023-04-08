cbc8a2a253b7409073d312068d3cb4d8dbac9982
smulch <mulcahy.samuel@gmail.com>
2023-04-07 23:14:19 -0400

    still cannot get my dang get to work right

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -7,7 +7,7 @@ const initdb = async () =>
         console.log('jate database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id' });
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: false });
       console.log('jate database created');
     },
   });
@@ -29,7 +29,7 @@ export const getDb = async () => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.getAll();
+  const request = store.get(1);
   const result = await request;
   console.log('result.value', result);
   return result;
